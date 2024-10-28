6629b6c67eaf8d0d11ab7fc68ab49bb138bfa77f
Ashraful Hoda <141621146+ashrafulHodaJs@users.noreply.github.com>
2024-10-28 14:45:39 +0530

    Update sample1.s

--- sample1.s
+++ sample1.s
@@ -12,26 +12,26 @@ main:
         push    rbp
         mov     rbp, rsp
         sub     rsp, 112
-        mov     edi, OFFSET FLAT:.LC0
+        mov     edi, .LC0
         mov     eax, 0
         call    printf
-        mov     rdx, QWORD PTR stdin[rip]
+        mov     rdx, QWORD PTR [rip]
         lea     rax, [rbp-112]
         mov     esi, 100
         mov     rdi, rax
         call    fgets
         lea     rax, [rbp-112]
-        mov     esi, OFFSET FLAT:.LC1
+        mov     esi, .LC1
         mov     rdi, rax
         call    strcspn
         mov     BYTE PTR [rbp-112+rax], 0
         lea     rax, [rbp-112]
-        mov     esi, OFFSET FLAT:.LC2
+        mov     esi, .LC2
         mov     rdi, rax
         call    strcmp
         test    eax, eax
         jne     .L2
-        mov     edi, OFFSET FLAT:.LC3
+        mov     edi, .LC3
         mov     eax, 0
         call    printf
         jmp     .L3
@@ -41,8 +41,8 @@ main:
 .L5:
         mov     edi, 1
         call    sleep
-        mov     edi, OFFSET FLAT:.LC4
-        call    puts
+        mov     edi, .LC4
+        call    puts  ;OR printf
         add     DWORD PTR [rbp-4], 1
 .L4:
         cmp     DWORD PTR [rbp-4], 49
